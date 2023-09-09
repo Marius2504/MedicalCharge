324f71f6a74c98607cc1f682882f46e0f5fcbf09
Marius2504 <marius.dumitrescu@s.unibuc.ro>
2023-09-09 22:09:30 +0300

    Adjusted parameters

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+def draw():
+    plt.plot(y_test, color='red', label='Real costs for medical operations')
+    plt.plot(y_pred, color='blue', label='Predicted cost for medical operations')
+    plt.title('Real costs')
+    plt.xlabel('Time')
+    plt.ylabel('Predicted costs')
+    plt.legend()
+    plt.show()
+
 
 dataset = pd.read_csv('insurance.csv')
 X = dataset.iloc[:, :-1].values
@@ -28,17 +40,31 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
+
 ann = tf.keras.models.Sequential()
-ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
-ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+
+NEURONS = 86
+NR_HL = 4
+
+for i in range(NR_HL):
+    ann.add(tf.keras.layers.Dense(units=NEURONS, activation='relu'))
+
+
+
 ann.add(tf.keras.layers.Dense(units=1))
 ann.compile(optimizer = 'adam', loss = 'mean_squared_error')
-ann.fit(X_train, y_train, batch_size = 32, epochs = 110)
+ann.fit(X_train, y_train, batch_size = 32, epochs = 140)
 
 y_pred=ann.predict(X_test)
 np.set_printoptions(precision=2)
-#print(np.concatenate((y_pred.reshape(len(y_pred),1),y_test.reshape(len(y_test),1)),1))
+print(np.concatenate((y_pred.reshape(len(y_pred),1),y_test.reshape(len(y_test),1)),1))
+y_pred=y_pred.reshape(len(y_pred),1)
+y_test=y_test.reshape(len(y_test),1)
+mse_test = mean_squared_error(y_test, y_pred)
+mae_test = mean_absolute_error(y_test, y_pred)
+r2_test = r2_score(y_test, y_pred)
 
-while true:
-    print("Predict charge for: ")
-    readline()
+draw()
+print(f'Mean Squared Error (MSE) on Test Data: {mse_test:.4f}')
+print(f'Mean Absolute Error (MAE) on Test Data: {mae_test:.4f}')
+print(f'R-squared (R2) on Test Data: {r2_test:.4f}')
